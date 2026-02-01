9bcf882725f09871a22661d732b0b88d0e97c0b4
doctorvictorandrews-tech <doctorvictorandrews@gmail.com>
2026-02-01 17:50:57 +0000

    Corrige estrutura de pastas

--- integra4-store/app/loja/produto/[slug]/page.tsx
+++ integra4-store/app/loja/produto/[slug]/page.tsx
@@ -2,9 +2,9 @@ import { getProduct } from "@/lib/shopify";
 import { notFound } from "next/navigation";
 import ProductDetails from "@/components/ProductDetails";
 
-// FORÇA O NEXT.JS A NÃO FAZER CACHE (Resolve o problema do produto fake)
+// Configuração obrigatória para o Cloudflare
 export const runtime = 'edge';
-export const dynamic = 'force-dynamic'; 
+export const dynamic = 'force-dynamic';
 
 export default async function ProductPage({ params }: { params: { slug: string } }) {
   const product = await getProduct(params.slug);
